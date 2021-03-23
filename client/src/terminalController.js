59a8a25a1b84694b65fc45272801d41928e75080
Halex Viotto Gomes <halexx002@gmail.com>
2021-03-23 15:31:08 -0400

    Hacker-chat project started and under construction

---
+++ client/src/terminalController.js
@@ -0,0 +1,103 @@
+import ComponentsBuilder from "./components.js";
+import { constants } from "./constants.js";
+
+export default class TerminalController {
+  #usersColors = new Map();
+
+  constructor() {}
+
+  #pickColor() {
+    return "#" + (((1 << 24) * Math.random()) | 0).toString(16) + "-fg";
+  }
+
+  #getUserColor(userName) {
+    if (this.#usersColors.has(userName)) return this.#usersColors.get(userName);
+
+    const color = this.#pickColor();
+    this.#usersColors.set(userName, color);
+
+    return color;
+  }
+
+  #onInputReceived(eventEmitter) {
+    return function () {
+      const message = this.getValue();
+      console.log(message);
+      this.clearValue();
+    };
+  }
+
+  #onMessageReceived({ screen, chat }) {
+    return (msg) => {
+      const { userName, message } = msg;
+      const color = this.#getUserColor(userName);
+      chat.addItem(`{${color}}{bold}${userName}{/}: ${message}`);
+      screen.render();
+    };
+  }
+
+  #onLogChanged({ screen, activityLog }) {
+    return (msg) => {
+      // erickwendel left
+      // erickwendel join
+
+      const [userName] = msg.split(/\s/);
+      const color = this.#getUserColor(userName);
+      activityLog.addItem(`{${color}}{bold}${msg.toString()}{/}`);
+
+      screen.render();
+    };
+  }
+
+  #onStatusChanged({ screen, status }) {
+    // ['erickwendel', 'mariazinha',...]
+    return (users) => {
+      // vamos pegar o primeiro elemento da lista
+      const { content } = status.items.shift();
+      status.clearItems();
+      status.addItem(content);
+
+      users.forEach((userName) => {
+        const color = this.#getUserColor(userName);
+        status.addItem(`{${color}}{bold}${userName}{/}`);
+      });
+
+      screen.render();
+    };
+  }
+
+  #registerEvents(eventEmitter, components) {
+    eventEmitter.on(
+      constants.events.app.MESSAGE_RECEIVED,
+      this.#onMessageReceived(components)
+    );
+    eventEmitter.on(
+      constants.events.app.ACTIVITYLOG_UPDATED,
+      this.#onLogChanged(components)
+    );
+    eventEmitter.on(
+      constants.events.app.STATUS_UPDATED,
+      this.#onStatusChanged(components)
+    );
+  }
+
+  async initializeTable(eventEmitter) {
+    const components = new ComponentsBuilder()
+      .setScreen({ title: "HackerChat - Hálex Viotto Gomes" })
+      .setLayoutComponent()
+      .setInputComponent(this.#onInputReceived(eventEmitter))
+      .setChatComponent()
+      .setActivityLogComponent()
+      .setStatusComponent()
+      .build();
+
+    this.#registerEvents(eventEmitter, components);
+
+    components.input.focus();
+    components.screen.render();
+
+    setInterval(() => {
+      console.log(this.#pickColor());
+    }, 1000);
+  }
+}
